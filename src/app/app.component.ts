a421ab24380efd7e05eed95f93f86083658606f9
Balram Chavan <balram.chavan@boehringer-ingelheim.com>
2019-01-25 20:03:46 +0100

    source code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multiple-env-demo';
+  environmentName = '';
+  environmentUrl = 'Debug api';
+
+  constructor() {
+    this.environmentName = environment.environmentName;
+    this.environmentUrl = environment.apiUrl;
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  environmentName: 'Production',
+  apiUrl: 'production url'
 };
